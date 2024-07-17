0e9e781d972d98ab6bf73821c8b6e52ab6b46741
Yuhyeon0516 <rladbgus0516@naver.com>
2024-07-17 20:25:16 +0900

    Created notifications app and connected notification module to payments module.

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,18 +1,24 @@
-import { CreateChargeDto } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
 import Stripe from 'stripe';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
   private readonly stripe = new Stripe(
     this.configService.get('STRIPE_SECRET_KEY'),
     { apiVersion: '2024-06-20' },
   );
 
-  async createCharge({ card, amount }: CreateChargeDto) {
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
     const paymentMethod = await this.stripe.paymentMethods.create({
       type: 'card',
       card,
@@ -42,6 +48,8 @@ export class PaymentsService {
     const paymentIntent = await this.stripe.paymentIntents.create(
       paymentIntentsCreateObject,
     );
+
+    this.notificationsService.emit('notify_email', { email });
 
     return paymentIntent;
   }
